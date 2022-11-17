98567922f0450fff1fafdce520302fbfa64bca06
Jasur1097 <jasur.asliev1097@gmail.com>
2022-11-17 22:22:12 +0500

    upd

--- weather.js
+++ weather.js
@@ -1,6 +1,6 @@
 "use strict"
 
-const url = "https://api.openweathermap.org/data/2.5/weather?q=Chicago&APPID=5500b9d99b57667e245407354f5537a0"
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Tashkent&APPID=5500b9d99b57667e245407354f5537a0"
 
 $.ajax ({
 	url: url,
