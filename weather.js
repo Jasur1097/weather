ada9f6e3f110a0d2dd68318e7716766f3a7b04e8
Jasur1097 <jasur.asliev1097@gmail.com>
2022-11-17 21:14:00 +0500

    javascript started

---
+++ weather.js
@@ -0,0 +1,17 @@
+"use strict"
+
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Chicago&APPID=5500b9d99b57667e245407354f5537a0"
+
+$.ajax ({
+	url: url,
+	success: function (result) {
+		console.log(result);
+		console.log(result.name);
+
+		$("#location").text(result.name);
+
+
+		$("#sky").text(result.weather[0].description);
+
+	}
+})
